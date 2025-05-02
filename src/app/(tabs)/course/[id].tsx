c0d91b9cf02738f5e063a5eedda78a911d9fb5d0
maxjvjohansson <max.jv.johansson@gmail.com>
2025-05-02 19:21:16 +0200

    Initiated react native maps and setup a view for a random golf hole

---
+++ src/app/(tabs)/course/[id].tsx
@@ -0,0 +1,37 @@
+import MapView, { Marker } from "react-native-maps";
+import { View, StyleSheet } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+export default function CoursePage() {
+  const { id } = useLocalSearchParams();
+
+  const tee = { latitude: 57.94176901908634, longitude: 12.031278555480554 };
+  const green = { latitude: 57.94077458061409, longitude: 12.03264098100342 };
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        zoomEnabled={true}
+        scrollEnabled={true}
+        pitchEnabled={true}
+        rotateEnabled={true}
+        mapType="satellite"
+        initialRegion={{
+          latitude: tee.latitude,
+          longitude: tee.longitude,
+          latitudeDelta: 0.003,
+          longitudeDelta: 0.003,
+        }}
+      >
+        <Marker coordinate={tee} title="Tee" />
+        <Marker coordinate={green} title="Green" />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: { flex: 1 },
+});
--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Router, useRouter } from "expo-router";
 
 export default function SearchPage() {
+  const router = useRouter();
+
   return (
     <SafeAreaView>
       <Text>Söksida</Text>
+      <TouchableOpacity onPress={() => router.push("/course/1")}>
+        <Text>Gå till bana</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
